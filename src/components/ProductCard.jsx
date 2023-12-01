72bda1824baeaad46690b40b49a6cf9dc7324fdd
Alexander_Ovchar <azalexxx02@gmail.com>
2023-12-01 03:07:53 +0200

    first commit

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const ProductCard = ({ product }) => {
+  const [description, setDescription] = useState(false);
+  console.log(description);
+  const toggle = () => {
+    setDescription((prev) => !prev);
+  };
+  return (
+    <div className="p-1 border border-gray-800 mb-2">
+      <h3>{product.title}</h3>
+      <p>{product.price}$</p>
+      <button onClick={toggle} class=" bg-slate-300 p-2">
+        toggle description
+      </button>
+      {description && <p>{product.description}</p>}
+    </div>
+  );
+};
+
+export default ProductCard;
